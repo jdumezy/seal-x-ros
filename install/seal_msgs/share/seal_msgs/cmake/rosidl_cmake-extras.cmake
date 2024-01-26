f501d35b58fb7befe3810781bce9f86aae20714d
jdumezy <jules.dumezy@gmail.com>
2024-01-26 17:49:51 +0100

    Removed build

--- install/seal_msgs/share/seal_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(seal_msgs_IDL_FILES "msg/SealData.idl;srv/KeyExchange.idl")
-set(seal_msgs_INTERFACE_FILES "msg/SealData.msg;srv/KeyExchange.srv;srv/KeyExchange_Request.msg;srv/KeyExchange_Response.msg")
